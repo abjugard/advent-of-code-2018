8f6e67fdfe2e5f595bf94af76cfb7f579d72f734
Adrian Bjugård <adrian@qng.se>
2018-12-08 01:33:12 +0100

    Improvements to day 3 solution

--- src/day03-no_matter_how_you_slice_it.py
+++ src/day03-no_matter_how_you_slice_it.py
@@ -1,47 +1,38 @@
 from santas_little_helpers import day, get_data, timed
 from collections import defaultdict
-from itertools import permutations
+from itertools import permutations, product
+import re
 
 today = day(2018, 3)
+r = re.compile(r'#(?P<claimer>\d+) @ (?P<x>\d+),(?P<y>\d+): (?P<w>\d+)x(?P<h>\d+)')
 
 def map_all_claims(data):
-  m = {}
-  for claimer, (x, y), area in data:
-    map_area(m, (x, y), area, claimer)
-  count = 0
-  for x in m.values():
-    for y, _ in x.values():
-      if y > 1:
-        count += 1
-  return count, m
+  m = defaultdict(lambda: defaultdict(lambda: (0, [])))
+  for claimer, coords, area in data:
+    map_area(claimer, coords, area, m)
+  return sum(1 for x in m.values() for y, _ in x.values() if y > 1), m
 
-def map_area(m, root, area, claimer):
+def map_area(claimer, root, area, m):
   x, y = root
-  xd, yd = area
-  for xp in range(x, x+xd):
-    if xp not in m:
-      m[xp] = defaultdict(lambda: (0, []))
-    for yp in range(y, y+yd):
-      count, claimers = m[xp][yp]
-      m[xp][yp] = count + 1, claimers + [claimer]
+  w, h = area
+  for xp, yp in product(range(x, x+w), range(y, y+h)):
+    count, claimers = m[xp][yp]
+    m[xp][yp] = count + 1, claimers + [claimer]
 
 def find_unique_claim(m, all_claimers):
-  for x in m.values():
-    for count, claimers in x.values():
-      if count > 1:
-        for claimer in claimers:
-          if claimer in all_claimers:
-            all_claimers.remove(claimer)
+  for claimers in [claimers for x in m.values() 
+                            for n, claimers in x.values() if n > 1]:
+    for claimer in [claimer for claimer in claimers 
+                            if claimer in all_claimers]:
+      all_claimers.remove(claimer)
   return all_claimers[0]
 
-
 def fun(line):
-  claimer, line = line.split(' @ ')
-  claimer = claimer[1:]
-  coords, area = line.split(': ')
-  coords = coords.split(',')
-  area = area.strip().split('x')
-  return claimer, (int(coords[0]), int(coords[1])), (int(area[0]), int(area[1]))
+  m = r.match(line)
+  claimer = m['claimer']
+  coords = int(m['x']), int(m['y'])
+  area = int(m['w']), int(m['h'])
+  return claimer, coords, area
 
 def main() -> None:
   data = list(get_data(today, [('func', fun)]))
