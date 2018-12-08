01b8b5cebae1ed495a50d72807c521a2d5dea76a
Adrian Bjugård <287697+abjugard@users.noreply.github.com>
2018-12-08 11:28:34 +0100

    Day 8 using iterators

--- src/day08-memory_maneuver.py
+++ src/day08-memory_maneuver.py
@@ -1,27 +1,29 @@
 from santas_little_helpers import day, get_data, timed
+from itertools import islice
 
 today = day(2018, 8)
 
 def parse_child(data):
-  child_count, meta_count, *data = data
+  child_count, meta_count = next(data), next(data)
+  if child_count == 0:
+    return [sum(islice(data, meta_count))] * 2
 
-  meta_total = 0
+  meta_total = meta_val = 0
   child_metas = []
   for _ in range(child_count):
-    child_sum, child_meta, data = parse_child(data)
+    child_sum, child_meta = parse_child(data)
     meta_total += child_sum
     child_metas += [child_meta]
 
-  metadata, data = data[:meta_count], data[meta_count:]
-  meta_total += sum(metadata)
-  meta_val = sum(metadata) if child_count is 0 else \
-             sum(child_metas[i - 1] for i in metadata
-                                    if i > 0 and i <= len(child_metas))
-  return meta_total, meta_val, data
+  for metadata in islice(data, meta_count):
+    meta_total += metadata
+    meta_val += child_metas[metadata - 1] \
+      if metadata > 0 and metadata <= child_count else 0
+  return meta_total, meta_val
 
 def main() -> None:
-  data = next(get_data(today, [('split', ' '), ('map', int)]))
-  star1, star2, _ = parse_child(data)
+  data = iter(next(get_data(today, [('split', ' '), ('map', int)])))
+  star1, star2 = parse_child(data)
 
   print(f'{today} star 1 = {star1}')
   print(f'{today} star 2 = {star2}')
