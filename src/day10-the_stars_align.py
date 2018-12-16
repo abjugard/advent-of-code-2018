9e650b86e889c30be910c59d2cce7f02649a0b9c
Adrian Bjugård <adrian@qng.se>
2018-12-16 10:55:53 +0100

    Day 10: Class representation for particles

--- src/day10-the_stars_align.py
+++ src/day10-the_stars_align.py
@@ -1,62 +1,77 @@
 from santas_little_helpers import day, get_data, timed, alphabet
 from itertools import product
 from importlib import import_module
+from operator import attrgetter as attr
 
 today = day(2018, 10)
 
-def calculate_sky_message(data):
-  points, velocities = [p[0] for p in data], [p[1] for p in data]
-  ydiff = first = abs(max(p[1] for p in points)-min(p[1] for p in points))
+class Particle(object):
+  def __init__(self, line):
+    self.x = int(line[10:16])
+    self.y = int(line[18:24])
+    self.vx = int(line[-8:-6])
+    self.vy = int(line[-5:-2])
+
+  def travel(self):
+    self.y += self.vy
+    self.x += self.vx
+
+  def __lt__(self, other):
+    return self.y < other.y
+
+  def __eq__(self, other):
+    return (self.y, self.x) == (other[1], other[0])
+
+  def ellipse(self, ymin, xmin, pixel_size):
+    yp = (self.y-ymin+2) * pixel_size
+    xp = (self.x-xmin+2) * pixel_size
+    return (xp, yp), (xp + pixel_size + 7, yp + pixel_size + 7)
+
+def calculate_sky_message(particles):
+  height = abs(max(particles).y - min(particles).y)
   time = 0
-  while ydiff > 10:
-    for index in range(len(points)):
-      points[index] = points[index][0] + velocities[index][0], points[index][1] + velocities[index][1]
-    ydiff = abs(max(p[1] for p in points)-min(p[1] for p in points))
+  while height > 10:
+    for particle in particles:
+      particle.travel()
+    height = abs(max(particles).y - min(particles).y)
     time += 1
-  ymax, ymin = max(p[1] for p in points), min(p[1] for p in points)
-  xmax, xmin = max(p[0] for p in points), min(p[0] for p in points)
+
+  ymax, ymin = max(particles).y, min(particles).y
+  xmax = max(particles, key=attr('x')).x
+  xmin = min(particles, key=attr('x')).x
 
   try:
-    return time, tesseract_parse(points)
+    return time, tesseract_parse(particles, ymax, ymin, xmax, xmin)
   except ImportError:
     for y in range(ymin, ymax+1):
       l = ''
       for x in range(xmin, xmax+1):
-        l += '█' if (x, y) in points else ' '
+        l += '█' if (x, y) in particles else ' '
       print(l)
     print()
     return time, None
 
-def tesseract_parse(points):
+def tesseract_parse(particles, ymax, ymin, xmax, xmin):
   Image = import_module('PIL.Image')
   ImageDraw = import_module('PIL.ImageDraw')
   pytesseract = import_module('pytesseract')
 
-  ymax, ymin = max(p[1] for p in points), min(p[1] for p in points)
-  xmax, xmin = max(p[0] for p in points), min(p[0] for p in points)
-
   pixel_size = 10
-  dimensions = ((xmax-xmin+5) * pixel_size, (ymax-ymin+5) * pixel_size)
+  dimensions = ((xmax - xmin+5) * pixel_size, (ymax - ymin+5) * pixel_size)
   img = Image.new('RGBA', dimensions, (255, 255, 255, 0))
   draw = ImageDraw.Draw(img)
-  for x, y in product(range(xmin, xmax+1), range(ymin, ymax+1)):
-    yp = (y-ymin+2) * pixel_size
-    xp = (x-xmin+2) * pixel_size
-    if (x, y) in points:
-      draw.ellipse(((xp, yp), (xp + pixel_size + 7, yp + pixel_size + 7)), fill='black')
-  result = pytesseract.image_to_string(img, config=f'--psm 6 -c tessedit_char_whitelist={alphabet.upper()}')
+  for particle in particles:
+    draw.ellipse(particle.ellipse(ymin, xmin, pixel_size), fill='black')
+  result = pytesseract.image_to_string(img, config='--psm 6')
   return result
 
-def fmt(line):
-  return (int(line[10:16]), int(line[18:24])), (int(line[-8:-6]), int(line[-5:-2]))
-
 def main() -> None:
-  points = list(get_data(today, [('func', fmt)]))
+  particles = list(get_data(today, [('func', Particle)]))
 
-  time, result = calculate_sky_message(points)
+  time, result = calculate_sky_message(particles)
   if result is None:
-    print('for cooler results, please install Pillow and pytesseract\n'\
-        +'(along with a tesseract-ocr distribution)\n')
+    print('for cooler results, please install Pillow and pytesseract\n' \
+        + '(along with a tesseract-ocr distribution)\n')
     print(f'{today} star 1 printed in block letters')
   else:
     print(f'{today} star 1 = {result}')
